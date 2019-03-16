db5d54b191d9a2c2379605e55c3cc21d7e8a048e
wangchaojian <wangchaojian@babytree-inc.com>
2019-03-16 21:44:25 +0800

    feat:transform-decorators-legacy add

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -404,7 +404,8 @@ module.exports = function(webpackEnv) {
                       libraryName: 'antd-mobile',
                       style: "css"
                     }
-                  ]
+                  ],
+                  [require.resolve("@babel/plugin-proposal-decorators"), { "legacy": true }]
                 ],
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
